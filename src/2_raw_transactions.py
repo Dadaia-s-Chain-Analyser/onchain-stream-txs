c5a2e2afdc16c6c59bbe57a599fc9598d0d720db
marcoaureliomenezes <marcoaurelioreislima@gmail.com>
2024-02-27 00:03:56 -0300

    Improving module structure and refactoring it to log the api_key consume to kafka

--- src/2_raw_transactions.py
+++ src/2_raw_transactions.py
@@ -18,79 +18,82 @@ from dm_logger import ConsoleLoggingHandler, KafkaLoggingHandler
 
 class RawTransactionsProcessor(BlockchainNodeAsAServiceConnector):
 
-    def __init__(self, network, api_key_node, vendor, logger):
-        self.network = network
-        self.api_key_node = api_key_node
-        self.web3 = self._get_node_connection(network, api_key_node, vendor)
-        self.utils = DataMasterUtils()
-        self.logger = logger
+  def __init__(self, network, logger, api_key, akv_secret_name, vendor):
+    self.network = network
+    self.api_key = api_key
+    self.akv_secret_name = akv_secret_name
+    self.web3 = self._get_node_connection(network, api_key, vendor)
+    self.utils = DataMasterUtils()
+    self.logger = logger
 
-    def consuming_topic(self, consumer):
-        while True:
-            msg = consumer.poll(timeout=0.1)
-            if msg: yield msg.value().decode('utf-8')
+  def consuming_topic(self, consumer):
+    while True:
+      msg = consumer.poll(timeout=0.1)
+      if msg: yield msg.value().decode('utf-8')
 
-    def process_msg(self, tx_id):
-        tx_data = self.web3.eth.get_transaction(tx_id)
-        tx_data = {k: bytes.hex(v) if type(v) == hexbytes.main.HexBytes else v for k, v in tx_data.items()}
-        if tx_data.get("accessList"):
-            tx_data["accessList"] = [dict(i) for i in tx_data["accessList"]]
-            tx_data["accessList"] = [self.utils.convert_hex_to_hexbytes(tx_data) for tx_data in tx_data["accessList"]]
-        return tx_data
+  def process_msg(self, tx_id):
+    tx_data = self.web3.eth.get_transaction(tx_id)
+    self.logger.info(f"API_request;{self.akv_secret_name}")
+    tx_data = {k: bytes.hex(v) if type(v) == hexbytes.main.HexBytes else v for k, v in tx_data.items()}
+    if tx_data.get("accessList"):
+      tx_data["accessList"] = [dict(i) for i in tx_data["accessList"]]
+      tx_data["accessList"] = [self.utils.convert_hex_to_hexbytes(tx_data) for tx_data in tx_data["accessList"]]
+    return tx_data
         
 
 if __name__ == '__main__':
 
-    network = os.environ["NETWORK"]
-    key_vault_node_name = os.environ['KEY_VAULT_NODE_NAME']
-    key_vault_node_secret = os.environ['KEY_VAULT_NODE_SECRET']
-    parser = argparse.ArgumentParser(description=f'Stream transactions network')
-    parser.add_argument('config_producer', type=argparse.FileType('r'), help='Config Producers')
-    parser.add_argument('config_consumer', type=argparse.FileType('r'), help='Config Consumers')
-    args = parser.parse_args()
+  network = os.environ["NETWORK"]
+  key_vault_node_name = os.environ['KEY_VAULT_NODE_NAME']
+  key_vault_node_secret = os.environ['KEY_VAULT_NODE_SECRET']
+  parser = argparse.ArgumentParser(description=f'Stream transactions network')
+  parser.add_argument('config_producer', type=argparse.FileType('r'), help='Config Producers')
+  parser.add_argument('config_consumer', type=argparse.FileType('r'), help='Config Consumers')
+  args = parser.parse_args()
 
-    # Parse de dados provenientes dos arquivos de configuração para variável config
-    config = ConfigParser()
-    config.read_file(args.config_producer)
-    config.read_file(args.config_consumer)
+  # Parse de dados provenientes dos arquivos de configuração para variável config
+  config = ConfigParser()
+  config.read_file(args.config_producer)
+  config.read_file(args.config_consumer)
 
-    # Nome de tópicos provenientes do arquivo de configuração
-    make_topic_name = lambda topic: f"{network}.{dict(config[topic])['topic']}"
-    topic_out_logs = make_topic_name('topic.app.logs')
-    topic_in_hash_id_txs = make_topic_name('topic.hash_txs')
-    topic_out_raw_txs = make_topic_name('topic.raw_txs')
+  # Nome de tópicos provenientes do arquivo de configuração
+  make_topic_name = lambda topic: f"{network}.{dict(config[topic])['topic']}"
+  topic_out_logs = make_topic_name('topic.app.logs')
+  topic_in_hash_id_txs = make_topic_name('topic.hash_txs')
+  topic_out_raw_txs = make_topic_name('topic.raw_txs')
 
-    # Criação de 1 consumer para o tópico de hash_txs e 1 producer para o tópico de raw_txs
-    create_producer = lambda special_config: Producer(**config['producer.general.config'], **config[special_config])
-    producer_txs_raw = create_producer('producer.raw_txs')
-    producer_logs = create_producer('producer.logs.block_clock')
-    consumer_txs_hash = Consumer(**config['consumer.general.config'], **config['consumer.hash_txs'])
-    consumer_txs_hash.subscribe([topic_in_hash_id_txs])
+  # Criação de 1 consumer para o tópico de hash_txs e 1 producer para o tópico de raw_txs
+  create_producer = lambda special_config: Producer(**config['producer.general.config'], **config[special_config])
+  producer_txs_raw = create_producer('producer.raw_txs')
+  producer_logs = create_producer('producer.logs.block_clock')
+  consumer_txs_hash = Consumer(**config['consumer.general.config'], **config['consumer.hash_txs'])
+  consumer_txs_hash.subscribe([topic_in_hash_id_txs])
 
-    # Configurando Logging para console e Kafka
-    logger = logging.getLogger("raw-transactions")
-    logger.setLevel(logging.INFO)
-    kafka_handler = KafkaLoggingHandler(producer_logs, topic_out_logs)
-    ConsoleLoggingHandler = ConsoleLoggingHandler()
-    logger.addHandler(ConsoleLoggingHandler)
-    logger.addHandler(kafka_handler)
+  # Configurando Logging para console e Kafka
+  logger = logging.getLogger("raw-transactions")
+  logger.setLevel(logging.INFO)
+  kafka_handler = KafkaLoggingHandler(producer_logs, topic_out_logs)
+  ConsoleLoggingHandler = ConsoleLoggingHandler()
+  logger.addHandler(ConsoleLoggingHandler)
+  logger.addHandler(kafka_handler)        
 
-    # Usa Azure Key vault para pegar as API KEYS guardadas lá como secrets.
-    credential = DefaultAzureCredential()
-    key_vault_api = KeyVaultAPI(key_vault_node_name, credential)
-    interval_keys = [int(i) for i in key_vault_node_secret.split("-")[-2:]]
-    name_secret = "-".join(key_vault_node_secret.split("-")[:-2])
-    vendor = key_vault_node_secret.split("-")[0]
-    api_keys = [key_vault_api.get_secret(f"{name_secret}-{i}") for i in range(interval_keys[0], interval_keys[1] + 1)]
-    api_key = api_keys[0]
+  # Usa Azure Key vault para pegar as API KEYS guardadas lá como secrets.
+  credential = DefaultAzureCredential()
+  key_vault_api = KeyVaultAPI(key_vault_node_name, credential)
+  interval_keys = [int(i) for i in key_vault_node_secret.split("-")[-2:]]
+  name_secret = "-".join(key_vault_node_secret.split("-")[:-2])
+  vendor = key_vault_node_secret.split("-")[0]
+  secret_name = f"{name_secret}-1"
+  api_keys = [key_vault_api.get_secret(f"{name_secret}-{i}") for i in range(interval_keys[0], interval_keys[1] + 1)]
+  api_key = api_keys[1]
 
-    # Inicializa processo de streaming com Source = hash_id_txs -> process -> Sink = raw_txs
-    raw_tx_processor = RawTransactionsProcessor(network, api_key, vendor, logger)
-    try:
-        for msg in raw_tx_processor.consuming_topic(consumer_txs_hash):
-            tx_data = raw_tx_processor.process_msg(msg)
-            logger.info(f"tx_processed;{tx_data['hash']};{tx_data['blockNumber']}")
-            encoded_message = json.dumps(tx_data).encode('utf-8')
-            producer_txs_raw.produce(topic_out_raw_txs, value=encoded_message)
-            producer_txs_raw.flush()
-    finally: consumer_txs_hash.close()
+  # Inicializa processo de streaming com Source = hash_id_txs -> process -> Sink = raw_txs
+  raw_tx_processor = RawTransactionsProcessor(network, logger, api_key, secret_name, vendor)
+  try:
+    for msg in raw_tx_processor.consuming_topic(consumer_txs_hash):
+      tx_data = raw_tx_processor.process_msg(msg)
+      encoded_message = json.dumps(tx_data).encode('utf-8')
+      producer_txs_raw.produce(topic_out_raw_txs, value=encoded_message)
+      producer_txs_raw.flush()
+      logger.info(f"mined_block_ingested;{tx_data['hash']};{tx_data['blockNumber']}")
+  finally: consumer_txs_hash.close()
--- src/3_transaction_classifier.py
+++ src/3_transaction_classifier.py
@@ -7,97 +7,69 @@ from dm_logger import ConsoleLoggingHandler, KafkaLoggingHandler
 
 class TransactionClassifier:
 
-    # Método construtor da classe TransactionClassifier
-    def __init__(self, network):
-        self.network = network
+  def __init__(self, network):
+    self.network = network
 
+  def consuming_topic(self, consumer):
+    while True:
+      msg = consumer.poll(timeout=0.1)
+      if msg: 
+        yield json.loads(msg.value())
 
-    # Método para classificar as transações e enviar para o Kafka ou EventHub
-    def __classify_transaction(self, transaction) -> None:
-       
-        if transaction['input'] == '': 
-            topic = f'{self.network}_simple_transaction'
-        elif transaction['to'] == None:
-            topic = f'{self.network}_contract_deployment'
-        else: 
-            topic = f'{self.network}_contract_interaction'
-        return topic, transaction 
-
-    def consuming_topic(self, consumer):
-        while True:
-            msg = consumer.poll(timeout=0.1)
-            if msg: yield json.loads(msg.value())
-
-
-    def process_msg(self, msg):
-
-        contract_deployment = lambda tx: tx['to'] == None
-        token_transfer = lambda tx: tx['input'] == ''
-        topic = 0 if token_transfer(msg) else 1 if contract_deployment(msg) else 2
-        return msg, topic
-            
-            # topic, transaction = self.__classify_transaction(tx)
-            # yield topic, transaction
+  def process_msg(self, msg, map_topics):
+    if msg['to'] == None: topic = '1'
+    elif msg['input'] == '': topic = '2'
+    else: topic = '3'
+    return msg, map_topics[topic]
 
 
 
 if __name__ == '__main__':
 
-    network = os.environ["NETWORK"]
+  network = os.environ["NETWORK"]
 
-    parser = argparse.ArgumentParser(description=f'Stream cleaned transactions network')
-    parser.add_argument('config_producer', type=argparse.FileType('r'), help='Config Producers')
-    parser.add_argument('config_consumer', type=argparse.FileType('r'), help='Config Consumers')
-    args = parser.parse_args()
+  parser = argparse.ArgumentParser(description=f'Stream cleaned transactions network')
+  parser.add_argument('config_producer', type=argparse.FileType('r'), help='Config Producers')
+  parser.add_argument('config_consumer', type=argparse.FileType('r'), help='Config Consumers')
+  args = parser.parse_args()
 
-    # Parse de dados provenientes dos arquivos de configuração para variável config
-    config = ConfigParser()
-    config.read_file(args.config_producer)
-    config.read_file(args.config_consumer)
-    
-    # Nome de tópicos provenientes do arquivo de configuração
-    make_topic_name = lambda topic: f"{network}.{dict(config[topic])['topic']}"
-    topic_in_raw_txs = make_topic_name('topic.raw_txs')
-    topic_out_nt_transfer = make_topic_name('topic.tx.native_token_transfer')
-    topic_out_sc_interaction = make_topic_name('topic.tx.contract_interaction')
-    topic_out_sc_deployment = make_topic_name('topic.tx.contract_deployment')
-    topic_out_logs = make_topic_name('topic.app.logs')
-
-    # Criação de 1 consumer para o tópico de hash_txs e 1 producer para o tópico de raw_txs
-    create_producer = lambda special_config: Producer(**config['producer.general.config'], **config[special_config])
-    producer_logs = create_producer('producer.logs.block_clock')
-    producer_classified_txs = create_producer('producer.classified_txs')
-    consumer_txs_hash = Consumer(**config['consumer.general.config'], **config['consumer.raw_txs'])
-    consumer_txs_hash.subscribe([topic_in_raw_txs])
-
-    # Configurando Logging para console e Kafka
-    logger = logging.getLogger("transaction-classifier")
-    logger.setLevel(logging.INFO)
-    kafka_handler = KafkaLoggingHandler(producer_logs, topic_out_logs)
-    ConsoleLoggingHandler = ConsoleLoggingHandler()
-    logger.addHandler(ConsoleLoggingHandler)
-    logger.addHandler(kafka_handler)
-
-    # Inicializa processo de streaming com Source = mined block event -> process -> Sinks = (block_metadata,raw_txs)
-    tx_classifier = TransactionClassifier(network)
-    list_topics = [topic_out_nt_transfer, topic_out_sc_interaction, topic_out_sc_deployment]
-    try:
-        for msg in tx_classifier.consuming_topic(consumer_txs_hash):
-            tx_data, topic_id = tx_classifier.process_msg(msg)
-            encoded_message = json.dumps(tx_data).encode('utf-8')
-            producer_classified_txs.produce(list_topics[topic_id], value=encoded_message)
-            producer_classified_txs.flush()
-            logger.info(f"tx_classified;{tx_data['hash'][:16]};topic={list_topics[topic_id]}")
-            
-    finally: consumer_txs_hash.close()
-
-
-    # kafka_client = KafkaClient(connection_str=kafka_host)
-    # producer = kafka_client.create_producer()
-    # consumer = kafka_client.create_consumer(consumer_group=group_id)
-    # consumer.subscribe([topic_consume])
-    # for topic, transaction in tx_classifier.classify_transactions(consumer):
-    #     kafka_client.send_data(producer, topic, transaction)
-    #     print(f"Transaction sent to {topic}")
-
+  # Parse de dados provenientes dos arquivos de configuração para variável config
+  config = ConfigParser()
+  config.read_file(args.config_producer)
+  config.read_file(args.config_consumer)
   
+  # Nome de tópicos provenientes do arquivo de configuração
+  make_topic_name = lambda topic: f"{network}.{dict(config[topic])['topic']}"
+  topic_in_raw_txs = make_topic_name('topic.raw_txs')
+  topic_out_nt_transfer = make_topic_name('topic.tx.native_token_transfer')
+  topic_out_sc_interaction = make_topic_name('topic.tx.contract_interaction')
+  topic_out_sc_deployment = make_topic_name('topic.tx.contract_deployment')
+  topic_out_logs = make_topic_name('topic.app.logs')
+
+  # Criação de 1 consumer para o tópico de hash_txs e 1 producer para o tópico de raw_txs
+  create_producer = lambda special_config: Producer(**config['producer.general.config'], **config[special_config])
+  producer_logs = create_producer('producer.logs.block_clock')
+  producer_classified_txs = create_producer('producer.classified_txs')
+  consumer_txs_hash = Consumer(**config['consumer.general.config'], **config['consumer.raw_txs'])
+  consumer_txs_hash.subscribe([topic_in_raw_txs])
+
+  # Configurando Logging para console e Kafka
+  logger = logging.getLogger("transaction-classifier")
+  
+  logger.setLevel(logging.INFO)
+  kafka_handler = KafkaLoggingHandler(producer_logs, topic_out_logs)
+  ConsoleLoggingHandler = ConsoleLoggingHandler()
+  logger.addHandler(ConsoleLoggingHandler)
+  logger.addHandler(kafka_handler)
+
+  # Inicializa processo de streaming com Source = mined block event -> process -> Sinks = (block_metadata,raw_txs)
+  tx_classifier = TransactionClassifier(network)
+  map_topics = {'1': topic_out_nt_transfer, '2': topic_out_sc_deployment, '3': topic_out_sc_interaction}
+  try:
+    for msg in tx_classifier.consuming_topic(consumer_txs_hash):
+      tx_data, topic = tx_classifier.process_msg(msg, map_topics)
+      encoded_message = json.dumps(msg).encode('utf-8')
+      producer_classified_txs.produce(topic, value=encoded_message)
+      producer_classified_txs.flush()
+      logger.info(f"raw_transaction_classified;{msg['hash'][:16]};topic={topic}")
+  finally: consumer_txs_hash.close()
